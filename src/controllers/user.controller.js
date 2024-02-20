1416cbff075aa9166efe69372d67a2e307a681aa
Plycedes <abhaypratapyadav340@gmail.com>
2024-02-21 00:51:19 +0530

    Middleware integration

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,19 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   res.status(200).json({
-    message: "ok",
+    message: "Gyatt",
   });
+
+  const { fullName, email, username, password } = req.body;
+  console.log("Name: ", fullName);
+
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "No field can be empty");
+  }
 });
 
 export { registerUser };
