a2ccfa1be8e8c9caaa5cba5e2275622ee8bf180d
Plycedes <abhaypratapyadav340@gmail.com>
2024-03-09 20:18:27 +0530

    changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -92,7 +92,7 @@ const registerUser = asyncHandler(async (req, res) => {
 const loginUser = asyncHandler(async (req, res) => {
   const { email, username, password } = req.body;
 
-  if (!usernmae || !email) {
+  if (!username && !email) {
     throw new ApiError(400, "username or email is required");
   }
 
@@ -143,8 +143,8 @@ const logoutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: {
-        refreshToken: undefined,
+      $unset: {
+        refreshToken: 1,
       },
     },
     {
