c86aeb3a244b9ea30a08a3c8634ead6237c7660c
Plycedes <abhaypratapyadav340@gmail.com>
2024-07-16 01:00:51 +0530

    Adding User Routes

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -36,6 +36,22 @@ router.route("/login").post(loginUser);
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
-router.route("/update-details").post(verifyJWT, updateAccountDetails);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+
+//get-routes
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
+
+//patch-routes
+router.route("/update-details").patch(verifyJWT, updateAccountDetails);
+router
+  .route("update-avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/update-cover-img")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+
+//params
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
 
 export default router;
